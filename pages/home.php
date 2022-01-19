e122125d5e34763446d42a03c23cad0494e8c692
Lupanov Dmitry <lupanov5@mail.ru>
2022-01-19 23:16:19 +0300

    19/01/2022

--- pages/home.php
+++ pages/home.php
@@ -8,33 +8,6 @@
         <ul class="city-list" data-city-list></ul>
     </div>
 
-    <div data-zone class="rate__wrapper">
-        <div data-markup class="rate__def">
-            <div class="rate__head">
-                <h5 class="rate__title">Москва</h5>
-                <a href="#" class="btn btn_lt rate__btn">Удалить</a>
-            </div>
-            <div  class="rate__head">
-                <span class="rate__title">Базовая стоимость доставки:</span>
-                <input type="text" class="nx-form-element rate__input" placeholder="320.00">
-                <a href="#" class="btn btn_lt rate__btn">Добавить наценку</a>
-            </div>
-            <ul class="rate-list">
-                <li class="rate-list__item">
-                    <div class="rate-list__block">
-                        <input type="text" class="nx-form-element rate__input">
-                        <span>кг -</span>
-                        <input type="text" class="nx-form-element rate__input">
-                    </div>
-                    <div class="rate-list__block">
-                        <input type="text" class="nx-form-element rate__input">
-                        <span>Итоговая стоимость доставки: </span>
-                    </div>
-                    <a href="#" class="btn btn_lt rate__btn">Удалить</a>
-                </li>
-            </ul>
-        </div>
-
-    </div>
+    <div data-zone class="rate__wrapper"></div>
 
 </section>
--- templates/_blocks/_search-line.php
+++
@@ -1,5 +0,0 @@
-<form action="/search/" class="nx-search-line">
-    <input type="text" name="q" class="nx-search-line__input" placeholder="Введите запрос">
-    <a href="#header-search" class="nx-search-line__close" data-search-toggle="close"><i class="mdi mdi-close"></i></a>
-    <a href="#" class="nx-search-line__search"><i class="mdi mdi-magnify"></i></a>
-</form>
